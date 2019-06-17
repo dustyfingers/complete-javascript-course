cc93672a6650fcfd109227df523e4e646cc3a734
dustyfingers <dustyfingers01257@gmail.com>
2019-06-16 18:24:18 -0600

    implemented simple dice roll

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -9,15 +9,30 @@ GAME RULES:
 
 */
 
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer;
 
 scores = [0, 0];
 roundScore = 0;
 activePlayer = 0;
-dice = Math.floor(Math.random() * 6) + 1;
-
-//document.querySelector(`#current-${activePlayer}`).textContent = dice; //not HTML!!
-document.querySelector(`#current-${activePlayer}`).innerHTML = `<em>${dice}</em>`; // js engine interprets html 
-document.querySelector('.dice').style.display = 'none';
 
 
+document.getElementById('score-0').textContent = 0;
+document.getElementById('score-1').textContent = 0;
+document.getElementById('current-0').textContent = 0;
+document.getElementById('current-1').textContent = 0;
+
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+    //anonymous func: you dont need to reference it anywhere else!
+
+    // 1. make dice random number
+    var dice = Math.floor(Math.random() * 6) + 1;
+
+    // 2. display result
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = `dice-${dice}.png`;
+
+
+    // 3. update the roundScore if rolled number !== 1
+});
